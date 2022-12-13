c58ada2339ead1a9cf3704f56b1caa000b06061c
Abdo <abdulrahman.elhosseini01@eng-st.cu.edu.eg>
2022-12-13 22:34:14 +0200

    Project instantiated

---
+++ Project/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public class Controller
+    {
+        DBManager dbMan;
+        public Controller()
+        {
+            dbMan = new DBManager();
+        }
+
+
+        public void TerminateConnection()
+        {
+            dbMan.CloseConnection();
+        }
+
+        //insert, update, delete query uses ExecuteNonQuery and return int
+        //select query ---> ExecuteReader and return datatable
+
+        //insert any query function here
+
+    }
+}
